2853492ecadb42db99422a47704b00cf573c888b
Agustin Thompson <htnitsuga@gmail.com>
2023-03-17 20:19:02 -0300

    comenzando

---
+++ JS orientado a Objetos/index.js
@@ -0,0 +1,15 @@
+console.log(typeof "Hola Mundo");   //string
+console.log(typeof 100);   //number
+console.log(typeof true);  //boolean
+console.log(typeof undefined);  //undefined
+console.log(typeof { });   //object
+console.log(typeof [ ]);   //object
+console.log('Hola Mundo');  //Hola Mundo
+console.log(2+56);
+console.log(2-56);
+
+
+
+
+
+
