7d773440433f6de504dceec85b9b5c64ef8ca828
Agustin Thompson <htnitsuga@gmail.com>
2023-03-17 20:49:20 -0300

    Crear Objetos

--- JS orientado a Objetos/index.js
+++ JS orientado a Objetos/index.js
@@ -4,9 +4,25 @@ console.log(typeof true);  //boolean
 console.log(typeof undefined);  //undefined
 console.log(typeof { });   //object
 console.log(typeof [ ]);   //object
-console.log('Hola Mundo');  //Hola Mundo
+console.log('Hola Mundo'.toUpperCase());  //Hola Mundo
 console.log(2+56);
 console.log(2-56);
+
+
+/* CREAR OBJETOS EN JS */
+
+const user = {
+    name: 'Juan',
+    secondName: 'Perez',
+    age: 30,
+    email: 'heeee@gatu.com',
+    address: { city: 'Bogota', zip: 12345, lat: 14.123, lng: 34.123 },
+    hobbies: ['music', 'movies', 'sports'],
+};
+
+console.log(user);
+
+
 
 
 
